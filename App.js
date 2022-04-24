668d70cc818573eeebced58615af67e09e57deb8
Lemniscate314 <issa.mboup314@gmail.com>
2022-04-24 16:33:06 -0400

    <fix> : login navigator

--- App.js
+++ App.js
@@ -5,32 +5,38 @@ import {Background_1} from './components/Background_1'
 import {HomePage,App2} from './screens/HomePage'
 import {ChoicesPage}  from './screens/ChoicesPage'
 import { NavigationContainer } from '@react-navigation/native';
-
+import { Login } from './screens/Login';
+import Background_2 from './components/Background_2';
+let log= Login
 let back = Background_1
+let back2 = Background_2
 let HP = HomePage
 let choice = ChoicesPage
-export  default class App extends React.Component {
+export   class App extends React.Component {
   render() {
   return <NavigationContainer> 
   <MyStack/>
 </NavigationContainer>;}
     
-}
+}   
 const Stack = createStackNavigator();
 const MyStack = () => 
 {
   return ( 
-    <Stack.Navigator initialRouteName='Home' screenOptions={{
+    <Stack.Navigator initialRouteName='Home' screenOptions={{ 
     headerShown: false,
     
   }}> 
       <Stack.Screen name = "Home" component = {HomePage}/>
 
       <Stack.Screen name = "Choices" component = {choice}/>
+     
+
+      <Stack.Screen name = "Login" component = {log}/>
     </Stack.Navigator>
   )
 }
-
+export  default back2
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -39,3 +45,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ 
