bb59c070b8bf3b2c06512ca6920a121cfbeca797
Antoine Ndiaye <antoinendiaye09@gmail.com>
2022-05-01 19:44:17 +0200

    <fix>: choices page conflict

--- screens/ChoicesPage.js
+++ screens/ChoicesPage.js
@@ -19,7 +19,6 @@ class ChoicesPage extends Component
     return ( 
       <><Background_1 />
         <View style = {styles.choix1}>
-        <Button color ="#fff" title= {this.state.BottomText} onPress = {() => this.props.navigation.navigate('Login')}/>
         <Button titleStyle = {styles.buttons}
                 buttonStyle = {{
                   backgroundColor: "",
