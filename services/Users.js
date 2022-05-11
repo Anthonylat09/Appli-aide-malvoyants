678f833a6b2cf65b8d4746ab9901162cbe96f8d9
Lemniscate314 <issa.mboup314@gmail.com>
2022-05-10 20:42:42 -0400

    <feat> : functions to communicate with Firebase real time database

---
+++ services/Users.js
@@ -0,0 +1,35 @@
+import {database }from 'firebase/database'
+import {ref,getDatabase,set}from 'firebase/database'
+export const db = getDatabase()
+
+export const AddUser = async (uid, name, email, image,location) => {
+
+    try {
+      const reference = ref(db,'/user' + uid)
+      console.log(reference)
+        set (reference,
+            {
+                name:name,
+                email:email,
+                image:image,
+                location:location 
+            }
+    
+        );
+    } catch (error) 
+    {
+        console.log(error);
+    }
+    }
+export const UpdateUserImage = async (image, uid) => {
+    try {
+        return await database
+            .getDatabase()
+            .ref("users/" + uid)
+            .update({
+                image:image
+            })
+    } catch (error) {
+        return error;
+    }
+}
